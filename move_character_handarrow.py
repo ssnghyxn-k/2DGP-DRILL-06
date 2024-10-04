cea1979161ad928c9e601b0c8506b6f7cc671083
ssnghyxn-k <kimsunghyun8312@gmail.com>
2024-10-04 17:54:05 +0900

    edit direction

--- move_character_handarrow.py
+++ move_character_handarrow.py
@@ -13,34 +13,43 @@ boy_y = TUK_HEIGHT // 2
 hand_x = random.randint(0, TUK_WIDTH)
 hand_y = random.randint(0, TUK_HEIGHT)
 face_right = True
+running = True
+frame = 0
+speed = 0.1
 
-while True:
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+
+while running:
     clear_canvas()
 
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     hand_arrow.draw(hand_x, hand_y)
 
-    if boy_x < hand_x:
-        boy_x += 5
-        face_right = True    
-    elif boy_x > hand_x:
-        boy_x -= 5
-        face_right = False
+    boy_x = (1 - speed) * boy_x + speed * hand_x
+    boy_y = (1 - speed) * boy_y + speed * hand_y
 
-    if boy_y < hand_y:
-        boy_y += 5
-    elif boy_y > hand_y:
-        boy_y -= 5
+    if boy_x < hand_x:
+        face_right = False
+    else:
+        face_right = True
 
     if face_right:
-        character.clip_draw(0,0,100,100,boy_x, boy_y)
+        character.clip_draw(frame * 100, 0, 100, 100, boy_x, boy_y)
     else:
-        character.clip_composite_draw(0, 0, 100, 100, 0, 'h', boy_x, boy_y, 100, 100)
+        character.clip_composite_draw(frame * 100, 0, 100, 100, 0, 'h', boy_x, boy_y, 100, 100)
 
     if abs(boy_x - hand_x) < 10 and abs(boy_y - hand_y) < 10:
         hand_x, hand_y = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
+    
+    frame = (frame + 1) % 8
 
     update_canvas()
-    delay(0.01)
+    handle_events()
+    delay(0.05)
 
 close_canvas()
