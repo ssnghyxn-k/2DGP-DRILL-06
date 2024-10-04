4b38acee9f31ebaa077cf5e45e2b809e07eb164b
ssnghyxn-k <kimsunghyun8312@gmail.com>
2024-10-04 17:56:05 +0900

    DRILL 06 제출 - 2020180007 김성현
    + add esc

--- move_character_handarrow.py
+++ move_character_handarrow.py
@@ -23,6 +23,9 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
 
 while running:
     clear_canvas()
